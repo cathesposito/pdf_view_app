0e8f66c8bacfb5c7df14dc30b150d8a61db4d6b4
Catarina Esposito Mendes <espositc@uoguelph.ca>
2023-04-25 13:04:04 -0400

    Fixture users.js

---
+++ src/users.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        key: 1,
+        name: "John",
+        address:
+            "12 John Av.",
+    },
+    {
+        key: 2,
+        name: "Scott",
+        address:
+            "13 Scott St.",
+    },
+    {
+        key: 3,
+        name: "David",
+        address:
+            "14 David Blv.",
+    },
+    {
+        key: 4,
+        name: "Sofia",
+        address:
+            "15 Sofia Hw."
+    }
+];
+
+export default data;
