4ea34f38cfafb39b5712b8297b3a5f25f2badc59
Catarina Esposito Mendes <espositc@uoguelph.ca>
2023-04-27 15:53:27 -0400

    Edit UserPDFView.jsx

--- src/components/UserPDFView.jsx
+++ src/components/UserPDFView.jsx
@@ -2,8 +2,82 @@ import React from "react";
 import { useParams } from 'react-router-dom';
 import data from "../users"
 
+import {
+    Document,
+    Page,
+    Text,
+    View,
+    StyleSheet,
+    PDFViewer,
+} from "@react-pdf/renderer";
 
-const UserPDFView = () => {
+// Create styles
+const styles = StyleSheet.create({
+    page: {
+        backgroundColor: "#fff",
+        color: "black",
+        fontFamily: "Helvetica",
+    },
+    section: {
+        margin: "10px",
+    },
+    h2: {
+        textDecoration: "underline",
+        textDecorationColor: "#D2C7FF",
+        fontSize: "24px",
+        marginBottom: "10px",
+    },
+    section_hr: {
+        marginTop: "-30px",
+        padding: "10px",
+        
+    },
+    line_hr: {
+        borderWidth: 1,
+        borderRightWidth: 0,
+        borderLeftWidth: 0,
+        borderTopWidth: 0,
+        opacity: 0.25,
+    },
+    viewer: {
+        width: window.innerWidth, //the pdf viewer will take up all of the width and height
+        height: window.innerHeight,
+    },
+    table: {
+        display: "table",
+        width: "30%",
+        borderStyle: "solid",
+        borderWidth: 1,
+        borderRightWidth: 0,
+        borderBottomWidth: 0
+    },
+    tableRow: {
+        margin: "auto",
+        flexDirection: "row"
+    },
+    tableCol: {
+        width: "50%",
+        borderStyle: "solid",
+        borderWidth: 1,
+        borderLeftWidth: 0,
+        borderTopWidth: 0
+    },
+    tableHeader: {
+        margin: "auto",
+        marginTop: 5,
+        fontSize: 12,
+        fontFamily: "Helvetica-Bold",
+    },
+    tableCell: {
+        margin: "auto",
+        marginTop: 5,
+        fontSize: 12,
+    }
+});
+
+// Create Document Component
+function UserPDFView() {
+
     const { key } = useParams();
 
     console.log(key);
@@ -13,33 +87,43 @@ const UserPDFView = () => {
     });
 
     return (
+        <PDFViewer style={styles.viewer}>
+            {/* Start of the document*/}
+            <Document>
+                {/*render a single page*/}
+                <Page size="A4" style={styles.page}>
+                    <View style={styles.section}>
+                        <Text style={styles.h2}>User {found.name}</Text>
+                        <Text>Address {found.address}</Text>
+                    </View>
+                    <View style={styles.section_hr}>
+                        <Text style={styles.line_hr}>{'\u00A0'}</Text>
+                    </View>
 
-        <div>
-            <h2>User {found.name}</h2>
-            <p>Address {found.address}</p>
+                    <View style={styles.section}>
+                        <View style={styles.table}>
+                            <View style={styles.tableRow}>
+                                <View style={styles.tableCol}>
+                                    <Text style={styles.tableHeader}>Name</Text>
+                                </View>
+                                <View style={styles.tableCol}>
+                                    <Text style={styles.tableHeader}>Address</Text>
+                                </View>
+                            </View>
+                            <View style={styles.tableRow}>
+                                <View style={styles.tableCol}>
+                                    <Text style={styles.tableCell}>{found.name}</Text>
+                                </View>
+                                <View style={styles.tableCol}>
+                                    <Text style={styles.tableCell}>{found.address}</Text>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                </Page>
 
-            <hr />
-
-            {found && (
-                <table className="table-user-view">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Address</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>{found.name}</td>
-                            <td>{found.address}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            )}
-
-        </div>
+            </Document>
+        </PDFViewer>
     );
-
-};
-
+}
 export default UserPDFView;
